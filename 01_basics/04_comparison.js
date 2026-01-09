7e521cd88ff8bd3e4bf09dbb1ce7bd50d6d83329
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-09 19:43:48 +0000

    comparsion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);          
+console.log(null >= 0); // true because equality check == and comparison >= < > works differently 
+
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined < 0) 
+
+// ===  strict equality operator , also checks the datatype
+
+console.log("2" === 2);
+
