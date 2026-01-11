e9cdf782e9e1e757fb3cd1c08d4569fe445041fa
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-11 09:56:38 +0000

    numbers and maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,40 @@
+// const score = 100
+// console.log(score); 
+
+// const balance = new Number(100)
+// console.log(balance); // Number {100} 
+
+// console.log(balance.toString().length) // 3
+// console.log(balance.toFixed(2)) // 100.00
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3)) // 124
+
+// const hundreds = 1000000 
+// console.log(hundreds.toLocaleString('en-IN')) // 10,00,000
+
+// ************************************ Maths ************************************
+
+// console.log(Math); 
+// console.log(Math.abs(-22)); // 22 
+// console.log(Math.round(4.3)); // 4
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.3)); // 5 
+// console.log(Math.floor(4.6)); // 4 
+// console.log(Math.min(2,34,5,6,-9,0)); // -9
+// console.log(Math.max(2,34,5,6,-9,0)); // 34 
+
+console.log(Math.random()); // 0 to 0.9999999 
+console.log(Math.random()*10); // 0 to 9.9999999 
+console.log((Math.random()*10) + 1); // 1 to 10.9999999 
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min ) 
+
+
+
+
+
+
