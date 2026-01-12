284c3d5b564427983034195ef8b2aecd2b2f729b
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-12 19:23:47 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -8,7 +8,7 @@ console.log(marvel_heros[3][1])
 const allHeros = marvel_heros.concat(dc_heros) // ways to merger arrays
 console.log(allHeros); 
 
-const all_new_heros = [...marvel_heros , ...dc_heros] // ways to merger arrays
+const all_new_heros = [...marvel_heros , ...dc_heros] // ways to merge arrays
 console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
