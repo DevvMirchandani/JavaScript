b241a62d5a62041e309d5b3b52fe1ffbf9dcc234
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-12 18:35:49 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,26 +1,32 @@
 const marvel_heros = ["thor" , "Ironman" , "spiderman"]
 const dc_heros = ["superman" , "flash" , "batman"]
 
-// marvel_heros.push(dc_heros)
-// console.log(marvel_heros)
-// console.log(marvel_heros[3][1])
+marvel_heros.push(dc_heros)
+console.log(marvel_heros)
+console.log(marvel_heros[3][1])
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+const allHeros = marvel_heros.concat(dc_heros) // ways to merger arrays
+console.log(allHeros); 
 
-const all_new_heros = [...marvel_heros , ...dc_heros]
-// console.log(all_new_heros);
+const all_new_heros = [...marvel_heros , ...dc_heros] // ways to merger arrays
+console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
 
-// console.log(real_another_array)
+console.log(real_another_array) // mereges and seprate each element in the array 
 
 console.log(Array.isArray("devkishan"))
 console.log(Array.from("devkishan"))
 
 console.log(Array.from({name: "devkishan"})) // empty array 
+
+let score1 = 100 
+let score2 = 200 
+let score3 = 300 
+
+console.log(Array.of(score1, score2 ,score3))
 
 
 
