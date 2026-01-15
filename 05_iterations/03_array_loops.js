1e73a8e0d0d06badfea2b27c79eb737e5332d80b
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-15 20:16:14 +0000

    array loops

---
+++ 05_iterations/03_array_loops.js
@@ -0,0 +1,44 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1 ,2 ,3 ,4 ,5]
+
+for (const num of arr) {
+    // console.log(num)
+} 
+
+const greetings = "Hello World ! "
+for (const greet of greetings) {
+    if (greet == " "){
+        continue
+    }
+    // console.log(`Each char is ${greet}`)
+}
+
+
+// Maps 
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")   // will not print it again because of it's unquiness of value
+
+// console.log(map)
+
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value)
+}
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of object) {
+//     console.log(key, ':-', value)                    // not working because Object is not iterable
+// }
