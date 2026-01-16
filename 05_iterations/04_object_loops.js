0483392464d503edba3e04135e0f92741150b044
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-16 13:45:05 +0000

    for in loop

---
+++ 05_iterations/04_object_loops.js
@@ -0,0 +1,30 @@
+// for in loop for object
+
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "Swift by Apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} value is for ${myObject[key]}`)
+}
+
+const programming = ["js", "rb", "cpp", "py", "java"]
+for (const key in programming) {
+//    console.log(programming[key]);
+   
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India") 
+
+// for (const key in map) {
+//     console.log(key)          // nothing will happen because map is not iterable
+// }
+
+
