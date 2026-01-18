19632a21e24fcece0462a95cda9548229990be57
Dev Mirchandani <mirchandanidevkishan123@gmail.com>
2026-01-18 15:10:33 +0000

    filters

--- 05_iterations/06_filters.js
+++ 05_iterations/06_filters.js
@@ -9,9 +9,9 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4                                       // reminder: return keyword is must if {} is open
-// })
+const newNums = myNums.filter( (num) => {
+    return num > 4                                       // reminder: return keyword is must if {} is open
+})
 
 // Or
 
